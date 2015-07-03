b068ce8fc6dc9ed4ff81bb30b449c8c93b049d81
Todor Balabanov <todor.balabanov@gmail.com>
2015-07-03 16:47:41 +0300

    Splash screen was added.

--- src/eu/veldsoft/hungarian/rings/HelpActivity.java
+++ src/eu/veldsoft/hungarian/rings/HelpActivity.java
@@ -2,8 +2,6 @@ package eu.veldsoft.hungarian.rings;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class HelpActivity extends Activity {
 
